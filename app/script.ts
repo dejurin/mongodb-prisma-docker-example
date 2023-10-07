0e6cf00832fce37cabd6f95dfbcd4dd2d9da2cff
YURII D <gkhelloworld@gmail.com>
2023-10-07 12:28:51 +0300

    add http and healthcheck

--- app/script.ts
+++ app/script.ts
@@ -1,21 +1,43 @@
-/* 
-*  https://www.prisma.io/docs/guides/migrate/seed-database#how-to-seed-your-database-in-prisma
-*/
-
 import { PrismaClient } from '@prisma/client'
+import * as http from 'http';
 
 const prisma = new PrismaClient()
 
 async function main() {
   const data = await prisma.users.findMany();
-  console.log({ data })
+  console.log(data);
+  return data; // Return the data
 }
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+
+const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.url === '/') {
+    try {
+      const data = await main();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ data }));
+    } catch (error) {
+      console.error(error);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
